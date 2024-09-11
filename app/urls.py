671adb84d9a91f6df6b222ed4a6ffc3aab080c77
sanchitapatel <sanchita15pa@gmail.com>
2024-09-10 18:22:03 -0700

    first commit

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from .views import *
 from django.urls import path
 urlpatterns=[
 path('set',set,name='set'),
+path("get",get,name='get'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def set(request):
-    data=render(request,'app/set.html')
-    data.set_cookies('name','sanchita')
-    #data.set_cookie('city','bhopal',httponly=True,)
+    data=render(request,'set.html')
+    data.set_cookie('name','sanchita')
+    data.set_cookie('city','bhopal',httponly=True,secure=False)
+    #data.set_cookie('name','sanchita',max_age=60*60*24,)
+    return data
+def get(request):
+    print(request.COOKIES)
+    name = request.COOKIES['name']
+    age = request.COOKIES['age']
+    city = request.COOKIES['city']
+    data={
+        'name':'nm',
+        'age':'ag',
+        'city':'city'
+    }
+    return render(request,'app/get.html',{'data':data})
+def delete(request):
+    data =render(request,'app/delete.html')
+    data.delete_cookie('name')
+    data.delete_cookie('age')
+    data.delete_cookie('city')
+    return data
