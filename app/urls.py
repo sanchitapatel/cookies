62849dcddc65a0c531f85c44322eb38806f550e9
sanchitapatel <sanchita15pa@gmail.com>
2024-09-14 07:43:00 -0700

    first commit

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,16 @@
-from .views import *
-from django.urls import path
-urlpatterns=[
-path('set',set,name='set'),
-path("get",get,name='get'),
+# from .views import *
+# from django.urls import path
+# urlpatterns=[
+# path('set',set,name='set'),
+# path("get",get,name='get'),
+# ]
+from django.contrib import admin
+from django.urls import path,include
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('set/', set, name='set'),
+    path('get/', get, name='get'),
+    # path('',include ('myapp.urls'))
 ]
--- app/views.py
+++ app/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def set(request):
+#     data=render(request,'set.html')
+#     data.set_cookie('name','sanchita')
+#     data.set_cookie('city','bhopal',httponly=True,secure=False)
+#     #data.set_cookie('name','sanchita',max_age=60*60*24,)
+#     return data
+# def get(request):
+#     print(request.COOKIES)
+#     name = request.COOKIES['name']
+#     age = request.COOKIES['age']
+#     city = request.COOKIES['city']
+#     data={
+#         'name':'nm',
+#         'age':'ag',
+#         'city':'city'
+#     }
+#     return render(request,'app/get.html',{'data':data})
+# def delete(request):
+#     data =render(request,'app/delete.html')
+#     data.delete_cookie('name')
+#     data.delete_cookie('age')
+#     data.delete_cookie('city')
+#     return data
+from django.shortcuts import render
+
+# Create your views here.
+
 def set(request):
-    data=render(request,'set.html')
-    data.set_cookie('name','sanchita')
-    data.set_cookie('city','bhopal',httponly=True,secure=False)
-    #data.set_cookie('name','sanchita',max_age=60*60*24,)
+    data = render(request, 'set.html')
+    data.set_cookie('name','Kushagra', max_age= 4*24*60*60, httponly=True,secure=True)
+    data.set_cookie('age','24', max_age= 4*24*60*60, httponly=True,secure=True)
+    data.set_cookie('city','Bhopal',max_age= 4*24*60*60, httponly=True,secure=False)
     return data
+
 def get(request):
     print(request.COOKIES)
     name = request.COOKIES['name']
     age = request.COOKIES['age']
     city = request.COOKIES['city']
-    data={
-        'name':'nm',
-        'age':'ag',
-        'city':'city'
-    }
-    return render(request,'app/get.html',{'data':data})
-def delete(request):
-    data =render(request,'app/delete.html')
-    data.delete_cookie('name')
-    data.delete_cookie('age')
-    data.delete_cookie('city')
-    return data
+    data={'name': name,
+          'age': age,
+          'city': city}
+    return render(request, 'get.html', data)
